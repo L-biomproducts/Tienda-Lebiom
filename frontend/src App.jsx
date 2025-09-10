4fd24869bb6ee55465d06e969ec9e6f75e5832bd
Tienda Lebiom <gerente.teziutlanlebiom@gmail.com>
2025-09-10 01:21:24 -0600

    Create src App.jsx

---
+++ frontend/src App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className='p-6'>
+      <h1 className='text-3xl font-bold'>Tienda Lebiom</h1>
+      <p>Bienvenido a la tienda en línea de productos Lebiom.</p>
+    </div>
+  );
+}
